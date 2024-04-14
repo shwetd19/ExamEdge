6d0e7804b5c56ca141232042622b09945662bd98
anuj <anujsbhuyar@gmail.com>
2024-04-14 13:29:47 +0530

    sidebar quiz student

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,14 +1,58 @@
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { BsLayoutSidebarInset } from "react-icons/bs";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 
-const Sidebar = () => {
+const SideBar = () => {
+  const [expanded, setExpanded] = useState(false);
+  const [showQuestions, setShowQuestions] = useState(false);
+
+  const isMobile = useMediaQuery("(max-width:600px)");
+
+  useEffect(() => {
+    if (expanded) {
+      const timeout = setTimeout(() => {
+        setShowQuestions(true);
+      }, 300);
+      return () => {
+        clearTimeout(timeout);
+        setShowQuestions(false);
+      };
+    }
+  }, [expanded]);
+
   return (
-    <div>
-      <div>
+    <div
+      // className={`flex flex-col h-full px-2 py-2 overflow-y-auto border-t border-r border-gray-300 transition-all duration-300 ${
+      //   expanded && isMobile ? "w-full" : ""
+      // }`}
+      className={`flex flex-col h-full px-2 py-2 overflow-y-auto border-t border-r border-gray-300 transition-all duration-300`}
+      style={{ width: expanded ? (isMobile ? "100%" : "400px") : "56px" }}
+    >
+      <div
+        onClick={() => setExpanded((p) => !p)}
+        className="w-8 h-8 p-2 flex justify-center items-center cursor-pointer hover:bg-gray-200 rounded-full"
+      >
         <BsLayoutSidebarInset size={18} fill="#444" />
+      </div>
+      <div className="overflow-y-auto mt-4">
+        <div className="items-center transition-all duration-300 cursor-pointer flex mb-4">
+          <p
+            // className={`w-8 h-8 ${
+            //   activeIndex === index
+            //     ? "bg-indigo-600 text-white"
+            //     : "bg-green-100 text-gray-900"
+            // } text-sm rounded-full flex items-center justify-center`}
+            className="w-8 h-8 bg-indigo-600 text-white text-sm rounded-full flex items-center justify-center"
+          >
+            1
+          </p>
+          {expanded && showQuestions && (
+            <p className="pl-4 w-10/12">quiz.title</p>
+          )}
+        </div>
       </div>
     </div>
   );
 };
 
-export default Sidebar;
+export default SideBar;
--- frontend/src/components/pages/StudentQuiz.jsx
+++ frontend/src/components/pages/StudentQuiz.jsx
@@ -1,11 +1,42 @@
-import { Sidebar } from "../Sidebar";
+import SideBar from "../SideBar";
+import Button from "@mui/material/Button";
 
 const StudentQuiz = () => {
   return (
     <div className="w-full flex flex-col flex-1 overflow-y-hidden h-[92vh]">
       <div className="flex flex-row flex-1 overflow-y-auto">
-        <Sidebar />
-        StudentQuiz
+        <SideBar />
+        <div className="flex-1  overflow-y-auto">
+          <div className="min-h-[8%] border-b border-t border-gray-300 flex px-4 py-4 justify-between">
+            <p className="mt-auto hidden sm:block">Question 1</p>
+            <div className="flex items-center justify-center flex-col sm:flex-row mt-auto">
+              <p className="sm:mr-4 mb-3 sm:mb-0 text-sm md:text-base">
+                {/* {response?.filter((resp) => resp.response !== "").length}/{" "} */}
+                {/* {data?.questions.length}  */}
+                Completed
+              </p>
+              <div className="bg-gray-200 rounded-full h-1 w-28 md:w-48 ">
+                <div
+                  className="bg-indigo-600 rounded-full h-1"
+                  style={{
+                    width: `${
+                      // (response?.filter((resp) => resp.response !== "").length /
+                      // data?.questions.length) *
+                      100
+                    }%`,
+                  }}
+                ></div>
+              </div>
+            </div>
+            <Button
+              variant="contained"
+              color="primary"
+              // onClick={onSubmit}
+            >
+              Submit
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
   );
